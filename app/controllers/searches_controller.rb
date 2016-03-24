d5d8f3d7b19354ea1a0cafc1d29e059f91b9ed53
benser1 <briganenser28@gmail.com>
2016-03-24 10:17:34 -0700

    API search working

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,30 @@
+class SearchesController < ApplicationController
+
+  def search 
+  end
+
+  def music
+    begin
+      @resp = Faraday.get "https://deezerdevs-deezer.p.mashape.com/search?q=" do |req|
+        req.headers['X-Mashape-Key'] = "NxfDHUyekSmshq3W54U90MH4gGAJp1iwq4AjsnG7UkT0t8urKA"
+        req.headers['Accept'] = 'text/plain'
+        req.params['q'] = params[:name]
+      end
+
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @search = body["data"]
+      # else
+      #   @error = body["meta"]["errorDetail"]
+      end
+
+    rescue Faraday::TimeoutError
+      @error = "There was a timeout. Please try again."
+    end
+    render 'search'
+  end
+
+
+
+
+end ## class end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE 
+
+  # ^^^^^^^^ Check to see if this raises security issues ^^^^^^^^^
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
